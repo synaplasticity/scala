84cc36ac5a87fa35f3a0f913aefe937b3093dbe0
synaplasticity <synaplasticity@gmail.com>
2017-08-17 12:17:46 +0530

    Removed unnecessary semi colons

--- Basics/src/main/scala/info/rewiring/basics/Point.scala
+++ Basics/src/main/scala/info/rewiring/basics/Point.scala
@@ -1,22 +1,22 @@
 package info.rewiring.basics
 
 class Point {
-  private var _x: Int = 0;
-  private var _y: Int = 0;
-  private val bound = 100;
+  private var _x: Int = 0
+  private var _y: Int = 0
+  private val bound = 100
   
-  def x = _x;
+  def x(): Int = _x
   
   def x_= (newValue: Int): Unit = {
     if (newValue < bound) _x = newValue else printWarning
   }
   
-  def y = _y
+  def y(): Int = _y
   
   def y_= (newValue: Int): Unit = {
     if (newValue < bound) _y = newValue else printWarning
   }
   
-  private def printWarning = println("WARNING: Out of bound")
+  private def printWarning(): Unit = println("WARNING: Out of bound")
 
 }
