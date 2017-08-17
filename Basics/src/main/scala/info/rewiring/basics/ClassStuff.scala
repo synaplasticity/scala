3fb17e2f6c84671eaab70c6966c1f011a381e023
synaplasticity <synaplasticity@gmail.com>
2017-08-17 15:26:34 +0530

    Scala class stuff tests

--- Basics/src/main/scala/info/rewiring/basics/ClassStuff.scala
+++ Basics/src/main/scala/info/rewiring/basics/ClassStuff.scala
@@ -4,13 +4,13 @@ import scala.beans.BeanProperty
 
 // NOTE: val needs to be added. Could be var too, but scala programmers do not like that.
 // @BeanProperty create java style get/setters (accessor/mutator). Helpful for interoperability
-class Employee(@BeanProperty firstName: String, val lastName: String, val title: String) {
-
+class Employee(@BeanProperty val firstName: String, val lastName: String, val title: String) {
   // ancillary constructors :
   // In scala the primary constructor is wide as we want to reduce object copy in
   // an immutable language
   // Unlike java and other OO languages the ancillary constructors are the once that
   // are more *narrow*
+  // this() call has to be the *first* call, else you will get a compilation error
   def this(firstName: String, lastName: String) = this(firstName, lastName, "Ms")
 
   //NOTE:
@@ -18,4 +18,24 @@ class Employee(@BeanProperty firstName: String, val lastName: String, val title:
   // This is not direct access to the field, but the method with parameters
   // which does not need to be explicitly declared.
   // NEEDS parameters on primary constrictor to be declared as VAL
+
+  // String interpolation. Like python "".format()
+  def fullName(): String = s"$firstName $lastName"
+
+  // Always go immutable. So in this case create a copy rather than change state
+  def changeLastName(ln: String) = new Employee(firstName, ln)
+
+  // default params
+  def copy(firstName: String = this.firstName,
+           lastName: String = this.lastName,
+           title: String = this.title): Employee = {
+
+    new Employee(firstName, lastName, title)
+  }
 }
+
+// You could also use default arguments. E.g.: For Title
+class Empl(@BeanProperty firstName: String, val lastName: String, val title: String = "Ms") {
+
+}
+
---
+++ Basics/src/test/scala/info/rewiring/basics/ClassStuffTests.scala
@@ -0,0 +1,44 @@
+package info.rewiring.basics
+
+import org.scalatest.FunSpec
+
+class ClassStuffTests extends FunSpec{
+
+  describe("Class stuff test suite") {
+
+    describe("Immutable ways to dealing with state change tests") {
+
+      it("should return full name with space (string interpolation)") {
+        val emp = new Employee("John", "Hartfield")
+        assert(emp.fullName() == "John Hartfield")
+      }
+
+      it("should return Ms as title (default argument value") {
+        val emp1 = new Employee("John", "Hartfield")
+        assert(emp1.title == "Ms")
+      }
+
+      it("should return a new object of Employee (immutable)") {
+        val emp2 = new Employee("John", "Hartfield")
+        val emp2Changed = emp2.changeLastName("Doe")
+
+        assert(!emp2.equals(emp2Changed))
+        assert(emp2Changed.lastName == "Doe")
+      }
+
+      it("should return a new object with new or default values") {
+        val emp3 = new Employee("John", "Hartfield")
+        val emp4 = emp3.copy("Jane", "Doe", "Miss")
+
+        assert(!emp3.equals(emp4))
+
+        assert(emp4.firstName == "Jane")
+        assert(emp4.lastName == "Doe")
+        assert(emp4.title == "Miss")
+      }
+    }
+
+
+
+  }
+}
